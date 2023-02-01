6e9efc420da2cdcbcdbb96565176859ea3a98300
Arishemm <arishem51@gmail.com>
2023-02-01 21:11:45 +0700

    feat: PostCSS

--- src/Provider/QueryProvider.tsx
+++ src/Provider/QueryProvider.tsx
@@ -1,9 +1,5 @@
 import React, { PropsWithChildren } from "react";
-import {
-  QueryClient,
-  QueryClientProvider,
-  QueryClientProviderProps,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient({
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>
+    <div className="py-2 bg-slate-800">
       <Link to="/">Header</Link>
       <Link to="/movies">Movies</Link>
     </div>
