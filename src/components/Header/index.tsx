ee31b32cd6cf9d800cbc53b09bf69edcbaa4121c
Arishemm <arishem51@gmail.com>
2023-02-01 21:20:33 +0700

    feat: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,18 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="py-2 bg-slate-800">
-      <Link to="/">Header</Link>
-      <Link to="/movies">Movies</Link>
+    <div className="py-4 bg-slate-800 ">
+      <nav className="md:container mx-auto flex items-center gap-12">
+        <Link
+          className="border-2 border-slate-50 p-1 text-xl text-slate-50 font-extrabold"
+          to="/"
+        >
+          Header
+        </Link>
+        <span className="text-md font-bold text-slate-50 cursor-pointer ">
+          Movies
+        </span>
+      </nav>
     </div>
   );
 }
