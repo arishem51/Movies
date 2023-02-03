81b4606ac454913b8f0d88955428f8a187d8de81
Arishemm <arishem51@gmail.com>
2023-02-03 20:01:38 +0700

    feat: Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,3 +1,11 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer className="p-4 bg-slate-900">
+      <h1 className="text-slate-50 font-bold text-xl text-center cursor-pointer">
+        <a target="_blank" href="https://github.com/arishem51/Movies">
+          Github
+        </a>
+      </h1>
+    </footer>
+  );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="py-4 bg-slate-800 ">
+    <div className="py-4 bg-slate-900 ">
       <nav className="md:container mx-auto flex items-center gap-12">
         <Link
           className="border-2 border-slate-50 p-1 text-xl text-slate-50 font-extrabold"
