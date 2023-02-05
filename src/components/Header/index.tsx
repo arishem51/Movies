2e95572d49b35367899fedf35f2db5d17e03d803
Arishemm <arishem51@gmail.com>
2023-02-05 12:29:07 +0700

    feat: update header animation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,29 @@
+import { motion, useAnimationControls, useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
+  const controls = useAnimationControls();
+  const { scrollY } = useScroll();
+  const scrollRef = useRef(scrollY.get());
+  useEffect(() => {
+    scrollY.on("change", (latest) => {
+      if (latest > scrollRef.current) {
+        controls.start({ y: "-100%" }, { duration: 0.5, delay: 0.2 });
+      } else if (latest < scrollRef.current) {
+        controls.start({ y: 0 }, { duration: 0.5, delay: 0.2 });
+      }
+      scrollRef.current = latest;
+    });
+    return () => {
+      controls.stop();
+    };
+  });
   return (
-    <div className="py-4 bg-slate-900 ">
+    <motion.div
+      animate={controls}
+      className="py-4 bg-slate-900 fixed left-0 right-0 z-10"
+    >
       <nav className="md:container mx-auto flex items-center gap-12">
         <Link
           className="border-2 border-slate-50 p-1 text-xl text-slate-50 font-extrabold"
@@ -14,6 +35,6 @@ export default function Header() {
           Movies
         </span>
       </nav>
-    </div>
+    </motion.div>
   );
 }
