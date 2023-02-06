83e1147e0ea7ec7d36e8c5d5e73f72f392d34795
Arishemm <arishem51@gmail.com>
2023-02-06 21:08:09 +0700

    feat: Header movies nav bar

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,8 +17,22 @@ export default function Header() {
         >
           Home
         </Link>
-        <span className="text-md font-bold text-slate-50 cursor-pointer ">
+        <span className="relative py-2 text-md font-bold text-slate-50 cursor-pointer group">
           Movies
+          <div className="absolute top-full hidden flex flex-col py-2 bg-slate-50 rounded whitespace-nowrap text-slate-900 group-hover:flex ">
+            <span className="font-normal py-1 pl-4 pr-16 hover:bg-slate-300">
+              Popular
+            </span>
+            <span className="font-normal py-1 pl-4 pr-16 hover:bg-slate-300">
+              Now Playing
+            </span>
+            <span className="font-normal py-1 pl-4 pr-16 hover:bg-slate-300">
+              Upcoming
+            </span>
+            <span className="font-normal py-1 pl-4 pr-16 hover:bg-slate-300">
+              Top Rated
+            </span>
+          </div>
         </span>
       </nav>
     </motion.div>
