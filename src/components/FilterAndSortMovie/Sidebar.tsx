39d5ff1717862e23f3ec7d39c952168c3815552d
Arishemm <arishem51@gmail.com>
2023-02-07 11:12:53 +0700

    feat: Sidebar

--- src/components/FilterAndSortMovie/Sidebar.tsx
+++ src/components/FilterAndSortMovie/Sidebar.tsx
@@ -14,6 +14,24 @@ export default function Sidebar() {
       <h1 className="font-semibold text-2xl">
         {SIDEBAR_HEADING[lastPathName] || "Movies"}
       </h1>
+      <Filter />
+    </div>
+  );
+}
+
+function Filter() {
+  return (
+    <div className="flex flex-col mt-4">
+      <ul className="shadow rounded border py-2 px-2 pr-12 cursor-pointer">
+        <h6 className="text-xl font-semibold">Filters</h6>
+        <li>
+          <div>
+            <h6 className="text-base text-slate-500">Generes</h6>
+            <div>123123123</div>
+          </div>
+        </li>
+      </ul>
+      <button className="btn btn-primary">Search</button>
     </div>
   );
 }
